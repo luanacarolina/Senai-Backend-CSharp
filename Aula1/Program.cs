98432584d3321dbffa56c718642ee60216b40650
luanacarolina <luanalcj1@hotmail.com>
2019-09-13 09:29:19 -0300

    add pasta

---
+++ Aula1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // Somente declaramos, sem atribuir valor
+        int num1;
+        //declaramos atribuindo um valor
+        int num2 =5;    
+        //declaramos diversas variaveis de mesmo tipo
+        int num3,num4,num5 =7;
+        //Variavel do tipo decimal (real)
+        float media;
+        //variavel do tipo logico
+        bool resultado =false;
+
+        Console.WriteLine(num2+num5);
+        }
+    }
+}
