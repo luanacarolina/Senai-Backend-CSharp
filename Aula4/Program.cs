b3bbb11c38c8679c6607db016d47a7f8f1e55e4e
luanacarolina <luanalcj1@hotmail.com>
2019-09-20 15:44:50 -0300

    inserção de pasta

---
+++ Aula4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double[] medias = new double[10];
+
+            for(int i=1;i<=10; i++){
+
+                Console.WriteLine("Digite uma média: ");
+                medias[i]=double.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine(medias[3]);
+            
+            for(int i = 0;i<medias.Length ;i++){
+            Console.WriteLine("Média : "+medias[i]);
+
+            }
+
+
+            }
+    }
+}
