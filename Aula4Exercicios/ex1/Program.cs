bb4876156c8249ab187b871fa7aae85dca1fff10
luanacarolina <luanalcj1@hotmail.com>
2019-09-20 15:41:09 -0300

    inserção de pastas

---
+++ Aula4Exercicios/ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[]valores = new double[10];
+
+            for(int i=0;i<=9;i++){
+                Console.WriteLine("Digite o valor " +i);
+                valores[i]=int.Parse(Console.ReadLine());
+            }
+            for(int i = 0;i<=9;i++){
+
+                Console.WriteLine(valores[i]);
+                Console.WriteLine("O ultimo valor multiplicado é : "+valores[9]*5);
+            }
+        }
+    }
+}
---
+++ Aula4Exercicios/ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        double[] valores1 = new double[10];
+        double[] valores2 = new double[10];
+        double[] valores3 = new double[20];
+        
+        for(int i=0;i<=9;i++){
+
+            Console.WriteLine("Digite o valor: ");
+            valores1[i]=double.Parse(Console.ReadLine());
+
+        }
+        for(int i =0;i<=9;i++){
+
+            Console.WriteLine("Digite o segundo valor :");
+            valores2[i]=double.Parse(Console.ReadLine());
+        
+        }
+         for(int i =0;i<=9;i++){
+        valores3[i]=valores1[i];
+        valores3[i+10]=valores2[i];
+      
+            }
+            for(int i = 0;i<=19;i++){
+                Console.WriteLine(valores3[i]);
+            }
+        }
+
+    }
+}
